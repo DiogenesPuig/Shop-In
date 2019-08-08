0f85c1721b532166177c5fe319ab46c2606c6742
Diogenes <diogenespuig@gmail.com>
2019-08-08 15:44:45 -0300

    Errores raros

--- src/Shop/Main.java
+++ src/Shop/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication19;
+package Shop;
 
 /**
  *
  * @author diogenes
  */
-public class JavaApplication19 {
+public class Main {
 
     /**
      * @param args the command line arguments
