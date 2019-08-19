559d866c5746a3c995ba091d5a82b8e0ee3a8f40
Diogenes <diogenespuig@gmail.com>
2019-08-19 12:47:52 -0300

    se creao la base para todas las clases

--- src/Shop/Usuario.java
+++ src/Shop/Usuario.java
@@ -10,5 +10,38 @@ package Shop;
  * @author diogenes
  */
 public class Usuario {
+    private String usuario;
+    private String pass;
+    private String telefono;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "usuario=" + usuario + ", pass=" + pass + ", telefono=" + telefono + '}';
+    }
+    
     
 }
