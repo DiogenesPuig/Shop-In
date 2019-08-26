3998ff3f890fb7c908ed259c1f8e2849afddc161
Diogenes <diogenespuig@gmail.com>
2019-08-26 19:06:17 -0300

    se crearon frames para hacer el iniciar sesion y registro (aun no funcionales)

--- src/Shop/Main.java
+++ src/Shop/Main.java
@@ -4,11 +4,7 @@
  * and open the template in the editor.
  */
 package Shop;
-import org.json.simple.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -26,31 +22,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JSONParser parser = new JSONParser();
-
-        try (Reader reader = new FileReader("/home/diogenes/Escritorio/Shop-in/productos.json")) {
-
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
-            System.out.println(jsonObject);
-
-            String name = (String) jsonObject.get("nombre");
-            System.out.println(name);
-
-            long age = (Long) jsonObject.get("codigo");
-            System.out.println(age);
-
-            // loop array
-            JSONArray msg = (JSONArray) jsonObject.get("messages");
-            Iterator<String> iterator = msg.iterator();
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        frm_menu.main(args);
 
     }
 
